a26ed89c79cd6e126366f2374e32c80e4077f784
Sai <67778413+saimanoj1234@users.noreply.github.com>
2023-04-12 07:19:51 -0700

    Update simple_dag.py

--- simple_dag.py
+++ simple_dag.py
@@ -28,5 +28,8 @@ with DAG(dag_id = "hello_world_dag",start_date = datetime(2023,1,1),
         task2 = BashOperator(task_id ="bashtask",bash_command = " echo hello world")
         task3 = PythonOperator(task_id ="pythontask",python_callable = my )
         task4 = DummyOperator(task_id = "end")
+
+        
+# Set up task dependencies
 task1 >> task2 >> task3 >> task4
 
